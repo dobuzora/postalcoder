481d9d3f13f3f47bd5a2025ec526f1ef9aeba38b
西山昴志 <e145735@ie.u-ryukyu.ac.jp>
2018-11-06 12:23:30 +0900

    add class

--- postalcoder.py
+++ postalcoder.py
@@ -1,39 +1,84 @@
-#!/usr/bin/env python
-# -*- coding: utf8 -*-
 import sys
-from tkinter import *
+import tkinter as tk
 
-fp = open("./okinawa-post/47OKINAW.CSV", "rt", encoding="shift_jis")
-for line in fp:
-    line = line.replace(' ', '')
-    line = line.replace('"', '')
-    cells = line.split(",")
-    zipno = cells[2] # 郵便番号
-    ken = cells[6] # 都道府県
-    shi = cells[7] # 市区
-    cho = cells[8] # 市区以下
-    title = ken + shi + cho
-    print(zipno + ":" + title)
-fp.close()
+class PostalCoder(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        master.title("postalcoder")
+        master.geometry("400x300")
+        self.pack()
+        self.create_widgets()
+        self.address = {}
+        self.input_data()
 
-root = Tk()
-root.title(u"postalcoder")
-root.geometry("400x300")
+    def input_data(self):
+        fp = open("./okinawa-post/47OKINAW.CSV", "rt", encoding="shift_jis")
+        for line in fp:
+            line = line.replace(' ', '')
+            line = line.replace('"', '')
+            cells = line.split(",")
+            zipno = cells[2] # 郵便番号
+            ken = cells[6] # 都道府県
+            shi = cells[7] # 市区
+            cho = cells[8] # 市区以下
+            title = ken + shi + cho
+            self.address[zipno] = title
+        fp.close()
 
-#ラベル
-Empty = Label(text=u'')
-Empty.pack()
-Static1 = Label(text=u'郵便番号を入力してください')
-Static1.pack()
+    def create_widgets(self):
+        # ラベル
+        self.label = tk.Label(self)
+        self.label["text"] = "\n郵便番号を入力してください"
+        self.label.pack()
 
-#エントリー
-EditBox = Entry()
-EditBox.pack()
+        # 入力フォーム
+        self.edit_box = tk.Entry(self)
+        self.edit_box.pack()
 
-#ボタン
-Button = Button(text=u'検索')
-Button.pack()
+        # 検索ボタンアクション
+        self.search = tk.Button(self)
+        self.search["text"] = "検索"
+        self.search["command"] = self.post_search
+        self.search.pack(side="top")
 
-root.mainloop()
+        # 警告用
+        self.warning = tk.Label(self)
+        self.warning["text"] = ""
+        self.warning.pack()
 
+        # 住所出力
+        self.ans = tk.Label(self)
+        self.ans["text"] = ""
+        self.ans.pack()
+        
+        # 終了アクション
+        self.quit = tk.Button(self, text="QUIT", fg="red", command=root.destroy)
+        self.quit.pack(side="bottom")
+
+    def post_search(self):
+        value = self.edit_box.get()
+        self.ans["text"] = self.validate(value)
+        self.ans.pack()
+
+    def validate(self, text):
+        try :
+            num = int(text)
+        except:
+            self.error("不適切な入力です")
+            return ""
+        
+        if text in self.address:
+            return self.address[text]
+        else : 
+            self.error("対応する住所がありません")
+            return ""
+
+    def error(self, msg):
+        print("[ERROR] ", msg)
+        self.warning["text"] = msg
+        self.warning.pack()
+
+root = tk.Tk()
+app = PostalCoder(master=root)
+app.mainloop()
 
